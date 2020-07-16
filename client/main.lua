124c6128e49f25c19795c76b3f02a25245e58d60
Rubylium <alexisoko0@gmail.com>
2020-07-16 14:37:29 +0200

    Init upload

---
+++ client/main.lua
@@ -0,0 +1,129 @@
+
+
+
+Citizen.CreateThread(function()
+    while true do
+        local pPed = GetPlayerPed(-1)
+        local pCoords = GetEntityCoords(pPed)
+        local nearGarage = false
+        for k,v in pairs(config.zone) do
+            if #(pCoords - v.pos) < 2.0 then
+                nearGarage = true
+                ShowHelpNotification("Press ~INPUT_PICKUP~ to open garage")
+                if IsControlJustReleased(0, 38) then
+                    OpenGarage(v)
+                end
+            end
+        end
+
+
+        if nearGarage then
+            Wait(1)
+        else
+            Wait(500)
+        end
+    end
+end)
+
+
+local menuOpen = false
+local vehicles = {}
+
+RegisterNetEvent("rFw:GetPlayerVehicles")
+AddEventHandler("rFw:GetPlayerVehicles", function(vehs)
+    vehicles = vehs
+end)
+
+RMenu.Add('garage', 'main', RageUI.CreateMenu("Garage", ""))
+RMenu:Get('garage', 'main'):SetSubtitle("~b~Garage action menu")
+RMenu:Get('garage', 'main').EnableMouse = false
+RMenu:Get('garage', 'main').Closed = function()
+    menuOpen = false
+end
+
+
+
+function OpenGarage(info)
+    if menuOpen then
+        menuOpen = false
+    else
+        menuOpen = true
+        RageUI.Visible(RMenu:Get('garage', 'main'), true)
+        TriggerServerEvent("rFw:GetPlayerVehicles")
+
+        Citizen.CreateThread(function()
+            while menuOpen do
+                RageUI.IsVisible(RMenu:Get('garage', 'main'), true, true, true, function()
+                    RageUI.Button("Ranger vehicle", "Store latest vehicle you were in or vehicle you are in.", true, function(Hovered, Active, Selected)
+                        if (Selected) then
+                            if IsPedInAnyVehicle(GetPlayerPed(-1), false) then
+                                local veh = GetVehiclePedIsIn(GetPlayerPed(-1), false)
+                                if veh ~= 0 then
+                                    local props = GetVehicleProperties(veh)
+                                    local plate = props.plate
+                                    TriggerServerEvent("rFw:SendVehToGarage", props, plate, VehToNet(veh))
+                                end
+                            else
+                                local veh = GetVehiclePedIsIn(GetPlayerPed(-1), true)
+                                if veh ~= 0 then
+                                    local props = GetVehicleProperties(veh)
+                                    local plate = props.plate
+                                    TriggerServerEvent("rFw:SendVehToGarage", props, plate, VehToNet(veh))
+                                end
+                            end
+                        end
+                    end)
+
+
+                    for k,v in pairs(vehicles) do
+                        if v.stored == 1 then
+                            RageUI.Button(GetDisplayNameFromVehicleModel(v.props.model).." ~b~["..v.props.plate.."]", nil, true, function(Hovered, Active, Selected)
+                                if (Selected) then
+                                    local spawn, heading = SelectRandomSpawn(info.out)
+                                    if spawn ~= false then
+                                        RequestModel(v.props.model)
+                                        while not HasModelLoaded(v.props.model) do Wait(1) end
+                                        local veh = CreateVehicle(v.props.model, spawn, heading, 1, 0)
+                                        SetVehicleProperties(veh, v.props)
+                                        SetEntityAsMissionEntity(veh, 1, 1)
+                                        if v.owned == 0 then
+                                            TriggerServerEvent("rFw:DeleteVehFromGarage", v.props.plate)
+                                        else
+                                            TriggerServerEvent("rFw:SetVehStatus", v.props.plate)
+                                        end
+                                    else
+                                        ShowNotification("No spawnpoint avalaibler.")
+                                    end
+                                end
+                            end)
+                        else
+                            RageUI.Button(GetDisplayNameFromVehicleModel(v.props.model).." ~b~["..v.props.plate.."] - ~r~OUT", nil, true, function(_, _, _)
+                            end)
+                        end
+                    end 
+            
+                end, function()
+                end)
+                Wait(1)
+            end
+        end)
+
+    end
+end
+
+
+function SelectRandomSpawn(zone)
+    local count = 0
+    for k,v in pairs(zone) do
+        count = count + 1
+        local r = zone[math.random(1, #zone)]
+        if IsSpawnPointClear(r.pos, 2.0) then
+            return r.pos, r.heading
+        end
+
+        if count >= #zone then
+            break
+        end
+    end
+    return false
+end
---
+++ server/main.lua
@@ -0,0 +1,82 @@
+function GetLicense(id)
+    local identifiers = GetPlayerIdentifiers(id)
+    for _, v in pairs(identifiers) do
+        if string.find(v, "license") then
+            return v
+        end
+    end
+end
+
+local playerVehCache = {}
+RegisterNetEvent("rFw:GetPlayerVehicles")
+AddEventHandler("rFw:GetPlayerVehicles", function()
+    local source = source
+    local license = GetLicense(source)
+    if playerVehCache[license] == nil then
+        local info = MySQL.Sync.fetchAll("SELECT owned, stored, props, mileage FROM players_veh WHERE owner = @identifier", {
+            ['@identifier'] = license
+        })
+
+        playerVehCache[license] = {}
+        for k,v in pairs(info) do
+            local decodedProps = json.decode(info[k].props)
+            playerVehCache[license][#playerVehCache[license] + 1] = {props = decodedProps, owned = info[k].owned, stored = info[k].stored, mileage = info[k].mileage}
+        end
+        TriggerClientEvent("rFw:GetPlayerVehicles", source, playerVehCache[license])
+    else
+        TriggerClientEvent("rFw:GetPlayerVehicles", source, playerVehCache[license])
+    end
+end)
+
+RegisterNetEvent("rFw:SendVehToGarage")
+AddEventHandler("rFw:SendVehToGarage", function(props, plate, net)
+    local source = source
+    local license = GetLicense(source)
+    local encodedProps = json.encode(props)
+
+    local info = MySQL.Sync.fetchAll("SELECT plate, owned FROM players_veh WHERE plate = @plate", {
+        ['@plate'] = plate 
+    })
+
+    print(info[1])
+    if info[1] == nil then
+        MySQL.Async.execute("INSERT INTO `players_veh` (`owner`, `owned`, `plate`, `stored`, `props`) VALUES ('"..license.."', '0', '"..plate.."', '1', '"..encodedProps.."')", {}, function()end)
+        playerVehCache[license][#playerVehCache[license] + 1] = {props = props, owned = 0, stored = 1, mileage = 0.0}
+        Citizen.InvokeNative(`DELETE_ENTITY` & 0xFFFFFFFF, NetworkGetEntityFromNetworkId(net))
+        TriggerClientEvent("rFw:GetPlayerVehicles", source, playerVehCache[license])
+    elseif info[1].owned == 1 then
+        -- Do an props update, compare encoded props in cache with new props
+        for k,v in pairs(playerVehCache[license]) do
+            if v.props.plate == plate then
+                playerVehCache[license][k].stored = 1
+            end
+        end
+        Citizen.InvokeNative(`DELETE_ENTITY` & 0xFFFFFFFF, NetworkGetEntityFromNetworkId(net))
+        TriggerClientEvent("rFw:GetPlayerVehicles", source, playerVehCache[license])
+    end
+end)
+
+RegisterNetEvent("rFw:DeleteVehFromGarage")
+AddEventHandler("rFw:DeleteVehFromGarage", function(plate)
+    local source = source
+    local license = GetLicense(source)
+    MySQL.Async.execute("DELETE FROM players_veh WHERE plate = '"..plate.."' AND owner = '"..license.."'", {}, function()end)
+    for k,v in pairs(playerVehCache[license]) do
+        if v.props.plate == plate then
+            playerVehCache[license][k] = nil
+        end
+    end
+    TriggerClientEvent("rFw:GetPlayerVehicles", source, playerVehCache[license])
+end)
+
+RegisterNetEvent("rFw:SetVehStatus")
+AddEventHandler("rFw:SetVehStatus", function(plate)
+    local source = source
+    local license = GetLicense(source)
+    for k,v in pairs(playerVehCache[license]) do
+        if v.props.plate == plate then
+            playerVehCache[license][k].stored = 0
+        end
+    end
+    TriggerClientEvent("rFw:GetPlayerVehicles", source, playerVehCache[license])
+end)
